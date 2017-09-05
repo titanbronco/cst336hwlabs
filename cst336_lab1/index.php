4cde02edb1c84db04f945d83099204c9525c4ace
Austin Gray <agray@csumb.edu>
2017-09-05 21:17:09 +0000

    leggo brinty

---
+++ cst336_lab1/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset= "utf-8"/>
+        <title>Austin Gray: Personal Website</title>
+        
+    
+        <link href ="css/styles.css" rel="stylesheet" type ="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Austin Gray </h1>
+        </header>
+        <nav>
+            <hr width="50%"/hr>
+            <a href = "index.html"><strong>Home</strong></a>
+            <a href = "about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /> <br />
+        
+        <main>
+            <figure id="me">
+                <img src = "img/augustine.jpg" alt = "Picture of Austin Gray" />
+            </figure>
+            
+            <div id="welcomeText">
+                Hello! <br />
+                
+                <p>
+                    Thank you for visiting my professional website!
+                </p>
+                
+                <p>
+                    I am a software engineering student at CSUMB.
+                </p>
+                
+                <p>
+                    <em>I appreciate your interest and will <strong>happily</strong> respond to any inquiries you have for me.</em>
+                </p>
+                
+                <br /> <br />
+            </div>
+        </main>
+        
+        
+        
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr> 
+            CST363. 2017&copy; Gray <br />
+            <strong>Disclaimer: </strong>The information in this web page is fictitious <br />
+            It is used for academic purposes only.
+            <img src = "img/csumbLogo.png" />
+            
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
